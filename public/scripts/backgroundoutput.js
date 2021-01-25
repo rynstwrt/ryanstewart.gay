1546ba47cdb94246403f140fc78b65033d6fcc4d
Ryan Stewart <ryanstewartalex@gmail.com>
2021-01-25 12:53:28 -0600

    Fix Webpack Issues

--- public/scripts/backgroundoutput.js
+++ public/scripts/backgroundoutput.js
@@ -19,14 +19,14 @@ eval("!function(e,t){ true?module.exports=t():0}(\"undefined\"!=typeof self?self
 
 /***/ }),
 
-/***/ "./js/background.ts":
-/*!**************************!*\
-  !*** ./js/background.ts ***!
-  \**************************/
+/***/ "./public/scripts/background.ts":
+/*!**************************************!*\
+  !*** ./public/scripts/background.ts ***!
+  \**************************************/
 /***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
 "use strict";
-eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nvar babylonjs_1 = __webpack_require__(/*! babylonjs */ \"./node_modules/babylonjs/babylon.js\");\r\nvar canvas = document.getElementsByTagName(\"canvas\")[0];\r\nvar engine = new babylonjs_1.Engine(canvas);\r\nvar scene = new babylonjs_1.Scene(engine);\r\nscene.clearColor = new babylonjs_1.Color4(0, 0, 0, 0);\r\nvar knot = babylonjs_1.Mesh.CreateTorusKnot(\"knot\", 7, 17, 1000, 10, 50, 50, scene);\r\nknot.scaling = babylonjs_1.Vector3.Zero();\r\nknot.receiveShadows = false;\r\nvar knotMat = new babylonjs_1.StandardMaterial(\"knotmat\", scene);\r\nknotMat.emissiveColor = babylonjs_1.Color3.FromHexString(\"#252525\");\r\nknotMat.wireframe = true;\r\nknot.material = knotMat;\r\nnew babylonjs_1.ArcRotateCamera(\"camera\", 0, Math.PI / 3, 500, knot.position, scene, true);\r\nvar doneScaling = false;\r\nvar doneFirstPopup = false;\r\nvar doneSecondPopup = false;\r\nengine.runRenderLoop(function () {\r\n    scene.render();\r\n    knot.rotation.x += .0010;\r\n    knot.rotation.y += .0010;\r\n    knot.rotation.z += .0010;\r\n    if (!doneScaling) {\r\n        if (knot.scaling.y >= 150) {\r\n            if (!doneFirstPopup) {\r\n                doneFirstPopup = true;\r\n                document.getElementById(\"welcome\").style.transform = \"scale(1)\";\r\n            }\r\n        }\r\n        if (knot.scaling.y >= 275) {\r\n            if (!doneSecondPopup) {\r\n                doneSecondPopup = true;\r\n                document.getElementById(\"welcome2\").style.transform = \"scale(1)\";\r\n            }\r\n        }\r\n        if (knot.scaling.y >= 300)\r\n            doneScaling = true;\r\n        else\r\n            knot.scaling = knot.scaling.add(new babylonjs_1.Vector3(1, 1, 1));\r\n    }\r\n});\r\n\n\n//# sourceURL=webpack://ryanstewart.gay/./js/background.ts?");
+eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nvar babylonjs_1 = __webpack_require__(/*! babylonjs */ \"./node_modules/babylonjs/babylon.js\");\r\nvar canvas = document.getElementsByTagName(\"canvas\")[0];\r\nvar engine = new babylonjs_1.Engine(canvas);\r\nvar scene = new babylonjs_1.Scene(engine);\r\nscene.clearColor = new babylonjs_1.Color4(0, 0, 0, 0);\r\nvar knot = babylonjs_1.Mesh.CreateTorusKnot(\"knot\", 7, 17, 1000, 10, 50, 50, scene);\r\nknot.scaling = babylonjs_1.Vector3.Zero();\r\nknot.receiveShadows = false;\r\nvar knotMat = new babylonjs_1.StandardMaterial(\"knotmat\", scene);\r\nknotMat.emissiveColor = babylonjs_1.Color3.FromHexString(\"#252525\");\r\nknotMat.wireframe = true;\r\nknot.material = knotMat;\r\nnew babylonjs_1.ArcRotateCamera(\"camera\", 0, Math.PI / 3, 500, knot.position, scene, true);\r\nvar doneScaling = false;\r\nvar doneFirstPopup = false;\r\nvar doneSecondPopup = false;\r\nengine.runRenderLoop(function () {\r\n    scene.render();\r\n    knot.rotation.x += .0010;\r\n    knot.rotation.y += .0010;\r\n    knot.rotation.z += .0010;\r\n    if (!doneScaling) {\r\n        if (knot.scaling.y >= 150) {\r\n            if (!doneFirstPopup) {\r\n                doneFirstPopup = true;\r\n                document.getElementById(\"welcome\").style.transform = \"scale(1)\";\r\n            }\r\n        }\r\n        if (knot.scaling.y >= 275) {\r\n            if (!doneSecondPopup) {\r\n                doneSecondPopup = true;\r\n                document.getElementById(\"welcome2\").style.transform = \"scale(1)\";\r\n            }\r\n        }\r\n        if (knot.scaling.y >= 300)\r\n            doneScaling = true;\r\n        else\r\n            knot.scaling = knot.scaling.add(new babylonjs_1.Vector3(1, 1, 1));\r\n    }\r\n});\r\n\n\n//# sourceURL=webpack://ryanstewart.gay/./public/scripts/background.ts?");
 
 /***/ })
 
@@ -71,7 +71,7 @@ eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\n
 /************************************************************************/
 /******/ 	// startup
 /******/ 	// Load entry module
-/******/ 	__webpack_require__("./js/background.ts");
+/******/ 	__webpack_require__("./public/scripts/background.ts");
 /******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "public/scripts/background.ts",
+    entry: "/public/scripts/background.ts",
     output: {
         filename: "backgroundoutput.js",
         path: path.resolve(__dirname, "public/scripts")
