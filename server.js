2d8776e1ecc9cd0044d54de1a1cde49deb7ebf2d
Ryan Stewart <ryanstewartalex@gmail.com>
2021-01-27 10:54:25 -0600

    a

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views")
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>
+{
+    res.render("index");
+});
+
+app.get("/about", (req, res) =>
+{
+    res.render("about");
+});
+
+app.get("/contact", (req, res) =>
+{
+    res.render("contact");
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () =>
+{
+   console.debug("listening on port " + port);
+});
+
+
+
+
