f85e44dde4e7d1fc28dd496cf08d394cd022441b
Ryan Stewart <ryanstewartalex@gmail.com>
2021-01-27 10:54:54 -0600

    Revert "a"
    This reverts commit 2d8776e1ecc9cd0044d54de1a1cde49deb7ebf2d.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    entry: "/public/scripts/background.ts",
+    output: {
+        filename: "backgroundoutput.js",
+        path: path.resolve(__dirname, "public/scripts")
+    },
+    resolve: {
+        extensions: [".ts"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    mode: "development"
+};
