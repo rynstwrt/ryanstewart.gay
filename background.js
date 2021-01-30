5d3c8781399fc1aa1a80b55ca01b806e4d5b5df4
Ryan Stewart <ryanstewartalex@gmail.com>
2021-01-29 19:43:10 -0600

    a

--- background.js
+++ background.js
@@ -1,56 +1,28 @@
 import * as BABYLON from "@babylonjs/core/Legacy/legacy.js";
 
-
 // canvas and engine setup
 const canvas = document.getElementById("bgcanvas");
 const engine = new BABYLON.Engine(canvas, true);
-
 
 // scene set up
 const scene = new BABYLON.Scene(engine);
 scene.clearColor = BABYLON.Color4.FromHexString("#00000000");
 
+// create shapes
+const latheShape = [new BABYLON.Vector3(0, 0, 0),
+    new BABYLON.Vector3(10, 5, 0),
+    new BABYLON.Vector3(5, 10, 0),
+    new BABYLON.Vector3(12, 15, 0),
+    new BABYLON.Vector3(3, 20, 0)];
+const lathe = BABYLON.MeshBuilder.CreateLathe("lathe", {shape: latheShape}, scene);
 
-// create cubes in shape of sine wave
-const cubes = [];
-for (let i = 0; i < 100; i += .1)
-{
-    const cube = BABYLON.MeshBuilder.CreateBox("box", {size: 2}, scene);
-    cube.position = new BABYLON.Vector3(0, Math.sin(i / 10) * 5, i);
-
-    const cubeMat = new BABYLON.StandardMaterial("cubemat", scene);
-    cubeMat.emissiveColor = BABYLON.Color3.FromHexString("#FF6600");
-
-    cube.material = cubeMat;
-
-    cubes.push(cube);
-}
-
+// create material
+const latheMat = new BABYLON.StandardMaterial("lathematerial", scene);
+latheMat.emissiveColor = BABYLON.Color3.FromHexString("#EF8354");
+lathe.material = latheMat;
 
 // create camera
-const camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -20), scene);
-camera.attachControl(true);
-
-
-// create animation
-const seconds = 10;
-const fps = 60;
-const anim = new BABYLON.Animation("anim", "position", fps, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE, true);
-
-const offset = new BABYLON.Vector3(0, 5, -10);
-const keys = [];
-for (let i = 0; i < cubes.length; ++i)
-{
-    keys.push({
-       frame: Math.round(i / (fps)),
-       value: cubes[i].position.add(offset)
-    });
-}
-console.log(keys);
-anim.setKeys(keys);
-camera.animations.push(anim);
-scene.beginAnimation(camera, 0, fps, false);
-
+const camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 60, lathe.position.add(new BABYLON.Vector3(0, 10, 0)), scene);
 
 // run render loop
 engine.runRenderLoop(() =>
