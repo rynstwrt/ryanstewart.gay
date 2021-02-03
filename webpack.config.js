a916f7e8af70485805c35b731aea740d840820f5
Ryan Stewart <ryanstewartalex@gmail.com>
2021-02-02 18:57:06 -0600

    Start Responsive Design

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
+const path = require("path");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+
 module.exports = {
-    entry: "cube.js"
+    entry: {
+        preloadbabylon: "/preloadbabylon.js",
+        cube: "/cube.js"
+    },
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "public/scripts"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    plugins: [new NodemonPlugin({
+        script: "./server.js",
+        watch: path.resolve("./public"),
+        ignore: ["*.js.map"],
+        ext: "js,njk,json",
+        delay: "500"
+    })]
 }
